e4efbc140c967912e6c60d8c64c261d48ac65f60
EricFeng19 <101969222+EricFeng19@users.noreply.github.com>
2024-02-03 13:02:52 +0800

    待檢查： reload實際上並未reload的問題

--- commands/setting.py
+++ commands/setting.py
@@ -6,19 +6,19 @@ from Core.classes import Cog_Extension
 
 class Setting(Cog_Extension):
     @commands.command()
-    async def reload(self, ctx, extension):
-        await self.bot.reload_extension(f'commands.{extension}') # self.bot
-        await ctx.send(f'Re - Loaded {extension} done.')
-
-    @commands.command()
-    async def reload(self, ctx, extension):
-        await self.bot.reload_extension(f'commands.{extension}') # self.bot
-        await ctx.send(f'Re - Loaded {extension} done.')
+    async def load(self, ctx, extension):
+        await self.bot.load_extension(f'commands.{extension}') # self.bot
+        await ctx.send(f'Loaded {extension} done.')
 
     @commands.command()
     async def unload(self, ctx, extension):
         await self.bot.unload_extension(f'commands.{extension}') # self.bot
         await ctx.send(f'Un - Loaded {extension} done.') 
+        
+    @commands.command()
+    async def reload(self, ctx, extension):
+        await self.bot.reload_extension(f'commands.{extension}') # self.bot
+        await ctx.send(f'Re - Loaded {extension} done.')
         
 async def setup(bot):
     await bot.add_cog(Setting(bot))
