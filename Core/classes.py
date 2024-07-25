bbfbb3c18301a1e678cfff8ad43235d476427b64
EricFeng19 <101969222+EricFeng19@users.noreply.github.com>
2024-07-25 21:41:22 +0800

    test, but still have problems

--- Core/classes.py
+++ Core/classes.py
@@ -2,5 +2,6 @@ import discord
 from discord.ext import commands
 
 class Cog_Extension(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, timeout=None):
         self.bot = bot
+        self.timeout = timeout
--- commands/button.py
+++ commands/button.py
@@ -2,15 +2,17 @@ import discord
 from discord.ext import commands
 from Core.classes import Cog_Extension
 
-class Button(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+class Button(Cog_Extension,discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+    def __init__(self):
+        super().__init__(timeout=None)  # 調用父類的初始化方法
 
-    @discord.ui.button(label="Click me!", style=discord.ButtonStyle.primary) # Create a button with the label "Click me!" with color Blurple
-    async def button_callback(self, button, interaction):
-        await interaction.response.send_message("You clicked the button!") # Send a message when the button is clicked
+    @discord.ui.button(label="Click me!", style=discord.ButtonStyle.blurple) # Create a button with the label "Click me!" with color Blurple
+    async def callback(self, interaction:discord.interactions, Button:discord.ui.Button):
+        await interaction.response.send(f'You clicked the button!') # Send a message when the button is clicked
 
-    @commands.slash() 
-    async def button(ctx):
-        await ctx.respond("This is a button!", view=Button()) # Send a message with our View class that contains the button
+    @commands.command(name = "buttonmenu") 
+    async def buttonmenu(self, ctx):
+        await ctx.respond(f'This is a button!', view = Button()) # Send a message with our View class that contains the button
 
 async def setup(bot):
     await bot.add_cog(Button(bot))
